b299ffb512b74c8bd1093ec645286a437e15e391
Andre Salata <andre.salata@rededor.com.br>
2024-07-18 16:46:36 -0300

    Adjusting python function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,12 +1,8 @@
 import json
-from extra_utils import get_lambda_mensagge
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Version v1.0.0')
+        'body': json.dumps('Hello from Lambda! Version V')
     }
-
-if __name__ == "__name__":
-    print(lambda_handler({}, {}))
